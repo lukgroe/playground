10be5b8e2b7d6a5ed150cdbd682ad7dc81144aeb
Lukas Gröber <lukasgrober@Lukass-MBP.fritz.box>
2021-02-06 00:44:01 +0100

    Added the most important functionalities

--- GoF_optimization.py
+++ GoF_optimization.py
@@ -1,15 +1,91 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import math
 
+# First: Set the key parameters for the described scenario
 
-size = 79;
-number_of_samples = 3;
-granularity = 10;
-epsilon = 0.000000000000001;
-infty= 1000000000000000;
+mu_0 = 0
+var_0 = 1
+mu_1 = 0.2
+var_1 = 0.8
+
+# Some constants for numerical purposes
+size = 50
+number_of_samples = 3
+granularity = 1500
+epsilon = 0.000000000000001
+infty= 1000000000000000
 distribution_support = np.linspace(-4, 4, granularity)
+uniform_support = np.linspace(0, 1, granularity)
+
+
+def create_plot_to_function(numbers, support=uniform_support, show=True):
+    plt.plot(support, numbers, 'ro', markersize=1, color='black')
+    if show:
+        plt.show()
+
+
+def map_number_on_support(number, support=uniform_support):
+    result = None
+    for i in support:
+        if number < i:
+            result = i
+            break
+    return result
+
+
+def map_array_on_support(numbers, support=uniform_support):
+    result = np.array([])
+    for i in numbers:
+        result = np.concatenate((result, np.array([map_number_on_support(i, support)])))
+    return result
+
+
+def empirical_df(numbers, support=uniform_support):
+    result = np.array([])
+    for time in support:
+        occur = [i for i in numbers if i <= time]
+        result = np.concatenate((result, np.array([len(occur)/len(numbers)])))
+    return result
+
+
+def normal_density(support, mu=0, var=1):
+    result = []
+    for time in support:
+        result += [(1 / (math.sqrt(2 * math.pi * var))) * (math.e ** (- ((time - mu) ** 2) / (2 * var)))]
+    result = np.array(result)
+    return result
+
+
+def integrate_function(numbers, support=uniform_support):
+    width = support[1]-support[0]
+    result =[]
+    old = 0
+    for i in numbers:
+        result = i
+    return result
+
+
+    return result
+
+
+def create_sample(size, distribution="uniform"):
+    if distribution == "uniform":
+        result = np.random.uniform(size=size, low=0, high=1)
+    return result
+
+
+check = create_sample(size)
+#print(sorted(check))
+#print(map_array_on_support(sorted(check)))
+#plt.ion()
+#create_plot_to_function(empirical_df(check))
 
 
 
+# plot sample, plot density
+create_plot_to_function(normal_density(distribution_support), distribution_support, False)
+create_plot_to_function(normal_density(distribution_support), distribution_support)
 
 
 """
